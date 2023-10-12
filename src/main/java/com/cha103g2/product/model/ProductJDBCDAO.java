57bd4f99874c2bb2a6086be8522f63481a9a0cfb
BO Ting Lee <a3325yyuacee@gmail.com>
2023-10-12 20:13:48 +0800

    修改ProductService, ProductServlet, ProductDAO, ProductVO 新增ProductJBDCDAO

--- src/main/java/com/cha103g2/product/model/ProductJDBCDAO.java
+++ src/main/java/com/cha103g2/product/model/ProductJDBCDAO.java
@@ -1,43 +1,37 @@
 package com.cha103g2.product.model;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
-public class ProductDAO implements ProductDAO_interface{
-	private static DataSource ds = null;
-//	static {
-//		try {
-//			Context ctx = new InitialContext();
-//			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/TestDB2");
-//		} catch (NamingException e) {
-//			e.printStackTrace();
-//		}
-//	}
+public class ProductJDBCDAO implements ProductDAO_interface {
+	String driver = "com.mysql.cj.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/cha103_g2?serverTimezone=Asia/Taipei";
+	String userid = "root";
+	String passwd = "811158tall";
 
 	private static final String INSERT_STMT = "INSERT INTO product(product_category_no, product_name, product_price, product_quantity, product_status, product_total_review_count, product_total_review_status) VALUES (?, ?, ?, ?, ?, ?, ?)";
 	private static final String UPDATE = "UPDATE product set product_category_no=?, product_name=?, product_price=?, product_quantity=?, product_status=?, product_total_review_count=?, product_total_review_status=? where product_no = ?";
 	private static final String DELETE = "DELETE FROM product where product_no = ?";
 	private static final String GET_ONE_STMT = "SELECT product_no,product_category_no,product_name,product_price,product_quantity,product_status,product_total_review_count,product_total_review_status FROM product where product_no = ?";
 	private static final String GET_ALL_STMT = "SELECT product_no,product_category_no,product_name,product_price,product_quantity,product_status,product_total_review_count,product_total_review_status FROM product order by product_no";
-	
+
 	@Override
 	public void insert(ProductVO productVO) {
 		// TODO Auto-generated method stub
-		//新增資料
+		// 新增資料
 		Connection con = null;
 		PreparedStatement pstmt = null;
-		
-		
+
 		try {
-			con = ds.getConnection();
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
 			pstmt = con.prepareStatement(INSERT_STMT);
-			
+
 			pstmt.setInt(1, productVO.getProductcategoryno());
 			pstmt.setString(2, productVO.getProductname());
 			pstmt.setInt(3, productVO.getProductprice());
@@ -45,55 +39,16 @@ public class ProductDAO implements ProductDAO_interface{
 			pstmt.setInt(5, productVO.getProductstatus());
 			pstmt.setInt(6, productVO.getProducttotalreviewcount());
 			pstmt.setInt(7, productVO.getProducttotalreviewstatus());
-			
+
 			pstmt.executeUpdate();
 
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
-			if(pstmt != null) {
-				try {
-					pstmt.close();
-				}catch(SQLException se){
-					se.printStackTrace(System.err);
-				}
-			}
-			if(con != null) {
-				try {
-					con.close();
-				}catch(Exception e) {
-					e.printStackTrace(System.err);
-				}
-			}
-		}
-		
-	}
-
-	@Override
-	public void update(ProductVO productVO) {
-		// TODO Auto-generated method stub
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			con = ds.getConnection();
-			pstmt = con.prepareStatement(UPDATE);
-			
-			pstmt.setInt(1, productVO.getProductcategoryno());
-			pstmt.setString(2, productVO.getProductname());
-			pstmt.setInt(3, productVO.getProductprice());
-			pstmt.setInt(4, productVO.getProductquantity());
-			pstmt.setInt(5, productVO.getProductstatus());
-			pstmt.setInt(6, productVO.getProducttotalreviewcount());
-			pstmt.setInt(7, productVO.getProducttotalreviewstatus());
-			
-			pstmt.executeUpdate();
-		} catch (SQLException se) {
-			throw new RuntimeException("A database error occured. "
-					+ se.getMessage());
-			// Clean up JDBC resources
-		}finally {
+		} finally {
 			if (pstmt != null) {
 				try {
 					pstmt.close();
@@ -109,7 +64,53 @@ public class ProductDAO implements ProductDAO_interface{
 				}
 			}
 		}
-		
+
+	}
+
+	@Override
+	public void update(ProductVO productVO) {
+		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(UPDATE);
+
+			pstmt.setInt(1, productVO.getProductcategoryno());
+			pstmt.setString(2, productVO.getProductname());
+			pstmt.setInt(3, productVO.getProductprice());
+			pstmt.setInt(4, productVO.getProductquantity());
+			pstmt.setInt(5, productVO.getProductstatus());
+			pstmt.setInt(6, productVO.getProducttotalreviewcount());
+			pstmt.setInt(7, productVO.getProducttotalreviewstatus());
+			pstmt.setInt(8, productVO.getProductno());
+
+			pstmt.executeUpdate();
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. " + se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
 	}
 
 	@Override
@@ -120,7 +121,8 @@ public class ProductDAO implements ProductDAO_interface{
 
 		try {
 
-			con = ds.getConnection();
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
 			pstmt = con.prepareStatement(DELETE);
 
 			pstmt.setInt(1, productno);
@@ -128,9 +130,11 @@ public class ProductDAO implements ProductDAO_interface{
 			pstmt.executeUpdate();
 
 			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
 		} catch (SQLException se) {
-			throw new RuntimeException("A database error occured. "
-					+ se.getMessage());
+			throw new RuntimeException("A database error occured. " + se.getMessage());
 			// Clean up JDBC resources
 		} finally {
 			if (pstmt != null) {
@@ -158,15 +162,16 @@ public class ProductDAO implements ProductDAO_interface{
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
-			con = ds.getConnection();
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
 			pstmt = con.prepareStatement(GET_ONE_STMT);
-			
+
 			pstmt.setInt(1, productno);
 
 			rs = pstmt.executeQuery();
-			
+
 			while (rs.next()) {
 				// productVo 也稱為 Domain objects
 				productVO = new ProductVO();
@@ -179,13 +184,15 @@ public class ProductDAO implements ProductDAO_interface{
 				productVO.setProducttotalreviewcount(rs.getInt("product_total_review_count"));
 				productVO.setProducttotalreviewstatus(rs.getInt("product_total_review_status"));
 			}
-				
+
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
 		} catch (SQLException se) {
 			// TODO Auto-generated catch block
-			throw new RuntimeException("A database error occured. "
-					+ se.getMessage());
+			throw new RuntimeException("A database error occured. " + se.getMessage());
 			// Clean up JDBC resources
-		}finally {
+		} finally {
 			if (rs != null) {
 				try {
 					rs.close();
@@ -207,7 +214,8 @@ public class ProductDAO implements ProductDAO_interface{
 					e.printStackTrace(System.err);
 				}
 			}
-		}return productVO;
+		}
+		return productVO;
 	}
 
 	@Override
@@ -219,12 +227,13 @@ public class ProductDAO implements ProductDAO_interface{
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
-			con = ds.getConnection();
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
 			pstmt = con.prepareStatement(GET_ALL_STMT);
 			rs = pstmt.executeQuery();
-			
+
 			while (rs.next()) {
 				// productVo 也稱為 Domain objects
 				productVO = new ProductVO();
@@ -238,13 +247,17 @@ public class ProductDAO implements ProductDAO_interface{
 				productVO.setProducttotalreviewstatus(rs.getInt("product_total_review_status"));
 				list.add(productVO); // Store the row in the list
 			}
-			
-		} catch (SQLException se) {
+
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. " + e.getMessage());
+			// Handle any SQL errors
+		}
+
+		catch (SQLException se) {
 			// TODO Auto-generated catch block
-			throw new RuntimeException("A database error occured. "
-					+ se.getMessage());
+			throw new RuntimeException("A database error occured. " + se.getMessage());
 			// Clean up JDBC resources
-		}finally {
+		} finally {
 			if (rs != null) {
 				try {
 					rs.close();
@@ -266,7 +279,65 @@ public class ProductDAO implements ProductDAO_interface{
 					e.printStackTrace(System.err);
 				}
 			}
-		}return list;
+		}
+		return list;
 	}
-	
+
+	public static void main(String[] args) {
+		ProductJDBCDAO dao = new ProductJDBCDAO();
+
+//				 查詢
+				ProductVO productVO3 = dao.findByPrimaryKey(303);
+				System.out.print("商品編號" + productVO3.getProductno() + ",\n");
+				System.out.print("商品名稱" + productVO3.getProductname() + ",\n");
+				System.out.print("商品價格" + productVO3.getProductprice() + ",\n");
+				System.out.print("商品庫存" + productVO3.getProductquantity() + ",\n");
+				System.out.print("商品是否上架" + productVO3.getProductstatus() + ",\n");
+				System.out.print("評價人數" + productVO3.getProducttotalreviewcount() + ",\n");
+				System.out.print("評價星數" + productVO3.getProducttotalreviewstatus() + ",\n");
+				System.out.println("商品類別編號" + productVO3.getProductcategoryno());
+				System.out.println("---------------------");
+//				 查詢
+//				List<ProductVO> list = dao.getAll();
+//				for (ProductVO aProduct : list) {
+//					System.out.print(aProduct.getProductno() + ",");
+//					System.out.print(aProduct.getProductname() + ",");
+//					System.out.print(aProduct.getProductprice() + ",");
+//					System.out.print(aProduct.getProductquantity() + ",");
+//					System.out.print(aProduct.getProductstatus() + ",");
+//					System.out.print(aProduct.getProducttotalreviewcount() + ",");
+//					System.out.print(aProduct.getProducttotalreviewstatus() + ",\t");
+//					
+//					System.out.print(aProduct.getProductcategoryno());
+//					System.out.println();
+//				}
+
+		// 新增
+//				ProductVO productVO1 = new ProductVO();
+//				
+//				productVO1.setProductname("巴黎釋迦");
+//				productVO1.setProductprice(1000);
+//				productVO1.setProductquantity(10);
+//				productVO1.setProductstatus(1);
+//				productVO1.setProducttotalreviewcount(0);
+//				productVO1.setProducttotalreviewstatus(0);
+//				productVO1.setProductcategoryno(6);
+//				dao.insert(productVO1);
+
+		// 修改
+//				ProductVO productVO2 = new ProductVO();
+//				productVO2.setProductno(301);
+//				productVO2.setProductname("celine");
+//				productVO2.setProductprice(14000);
+//				productVO2.setProductquantity(100);
+//				productVO2.setProductstatus(1);
+//				productVO2.setProducttotalreviewcount(1);
+//				productVO2.setProducttotalreviewstatus(1);
+//				productVO2.setProductcategoryno(7);
+//				dao.update(productVO2);
+
+		// 刪除
+//				dao.delete(311);
+
+	}
 }
