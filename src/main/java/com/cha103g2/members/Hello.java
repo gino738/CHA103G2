4b12c6c383780f2c1a6b9d7e2ea1a1df66772c32
Sammi Hong <shia0406@gmail.com>
2023-10-13 21:14:18 +0800

    commit members

--- src/main/java/com/cha103g2/members/Hello.java
+++
@@ -1,8 +0,0 @@
-package com.cha103g2.members;
-
-public class Hello {
-
-	public static void main(String[] args) {
-		System.out.print("hello");
-	}
-}
--- src/main/java/com/cha103g2/members/model/Members.java
+++ src/main/java/com/cha103g2/members/model/Members.java
@@ -1,27 +1,59 @@
-package com.cha103g2.members;
+package com.cha103g2.members.model;
 
-import java.io.Serializable;
 import java.sql.Date;
 
-public class Members implements Serializable {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "members")
+public class Members {
+	
+	@Id
+	@Column(name = "mem_no") // 若變數名稱和欄位名稱相同可以不用寫，但建議還是要寫
 	private Integer memno;
-	private boolean memstatus;
+
+	@Column(name = "mem_status")
+	private Boolean memstatus;
+
+	@Column(name = "mem_name")
 	private String memname;
+	
+	@Column(name = "mem_mail")
 	private String memmail;
+
+	@Column(name = "mem_account")
 	private String memaccount;
+	
+	@Column(name = "mem_pass")
 	private String mempass;
+
+	@Column(name = "mem_gender")
 	private Integer memgender;
+	
+	@Column(name = "mem_id")
 	private String memid;
+
+	@Column(name = "mem_bir")
 	private Date membir;
+
+	@Column(name = "mem_phone")
+	private String memphone;
+
+	@Column(name = "mem_address")
 	private String memaddress;
+	
+	@Column(name = "mem_date")
 	private Date memdate;
 	
 	public Members() {
-
+		super();
 	}
 
 	public Members(Integer memno, boolean memstatus, String memname, String memmail, String memaccount, String mempass,
-			Integer memgender, String memid, Date membir, String memaddress, Date memdate) {
+			Integer memgender, String memid, Date membir,String memphone ,String memaddress, Date memdate) {
 		this.memno = memno;
 		this.memstatus = memstatus;
 		this.memname = memname;
@@ -31,10 +63,13 @@ public class Members implements Serializable {
 		this.memgender = memgender;
 		this.memid = memid;
 		this.membir = membir;
+		this.memphone = memphone;
 		this.memaddress = memaddress;
 		this.memdate = memdate;
 	}
 
+	
+	
 	public Integer getMemno() {
 		return memno;
 	}
@@ -43,11 +78,11 @@ public class Members implements Serializable {
 		this.memno = memno;
 	}
 
-	public boolean isMemstatus() {
+	public Boolean getMemstatus() {
 		return memstatus;
 	}
 
-	public void setMemstatus(boolean memstatus) {
+	public void setMemstatus(Boolean memstatus) {
 		this.memstatus = memstatus;
 	}
 
@@ -107,6 +142,14 @@ public class Members implements Serializable {
 		this.membir = membir;
 	}
 
+	public String getMemphone() {
+		return memphone;
+	}
+
+	public void setMemphone(String memphone) {
+		this.memphone = memphone;
+	}
+
 	public String getMemaddress() {
 		return memaddress;
 	}
@@ -122,5 +165,7 @@ public class Members implements Serializable {
 	public void setMemdate(Date memdate) {
 		this.memdate = memdate;
 	}
+
+	
 
 }
