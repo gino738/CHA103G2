f253deba19ba628c50bf9a793c1d467ebd0bbe41
BOT <a3325yyuacee@gmail.com>
2023-10-15 23:31:33 +0800

    測試 刪除Hello 新增Pen

--- src/main/java/com/cha103g2/productCategory/Hello.java
+++
@@ -1,8 +0,0 @@
-package com.cha103g2.productCategory;
-
-public class Hello {
-
-	public static void main(String[] args) {
-		System.out.print("hello");
-	}
-}
---
+++ src/main/java/com/cha103g2/productCategory/entity/Pen.java
@@ -0,0 +1,14 @@
+package com.cha103g2.productCategory.entity;
+
+public class Pen {
+	private String brand;
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+}
